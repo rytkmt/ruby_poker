04e2b69868bf75019362a833a30756c548d96681
r_tsukamoto <r_tsukamoto@ill.co.jp>
2020-07-22 21:35:45 +0900

    Add test-unit

--- test/ruby_poker_test.rb
+++ test/ruby_poker_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class RubyPokerTest < Minitest::Test
+class RubyPokerTest < Test::Unit::TestCase
   def test_that_it_has_a_version_number
     refute_nil ::RubyPoker::VERSION
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "ruby_poker"
 
-require "minitest/autorun"
+require "test/unit"
