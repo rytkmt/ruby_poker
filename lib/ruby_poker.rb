df4dce0942e596b209107dc524c45e9c96c0d6d1
r_tsukamoto <r_tsukamoto@ill.co.jp>
2020-07-23 00:01:14 +0900

    Use reverse

--- lib/ruby_poker.rb
+++ lib/ruby_poker.rb
@@ -3,5 +3,6 @@ require "ruby_poker/card"
 
 module RubyPoker
   NUMBERS = ([*2..13] + [1]).freeze
-  SUITS = %i[club diamond heart spade].freeze
+  # indexで比較するため弱い順で定義を行うが、見た目上強い順に定義してreverseする
+  SUITS = %i[spade heart diamond club].reverse.freeze
 end
